f82c91df488d8a7b6d86c72ceafc35884a313614
dntandan <deepnarayan006@gmail.com>
2021-10-31 20:37:41 +0530

    updated index page with card and layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,74 @@
 import Head from 'next/head'
 import DarkToggle from '@/components/DarkToggle'
+import Layout from '@/components/Layout'
+import Card from '@/components/Card'
 
-export default function Home() {
+const FeaturedProducts = [
+  {
+      title: "Chhath Theme Based Tshirt (Hindi)",
+      price: "400",
+      cost: "800",
+      off: "50",
+      imageSrc: "products/t-shirt-festival.jpeg",
+      sizes:[{size:'S'},{size:'M'},{size:'XXL'},],
+
+  },
+  {
+      title: "Chhath Theme Based Tshirt (Hindi) 1",
+      price: "400",
+      cost: "800",
+      off: "50",
+      imageSrc: "products/t-shirt-festival.jpeg",
+      sizes:[{size:'S'},{size:'L'},{size:'XL'},],
+  },
+  {
+      title: "Chhath Theme Based Tshirt (Hindi) 2",
+      price: "400",
+      cost: "800",
+      off: "50",
+      imageSrc: "products/t-shirt-festival.jpeg",
+      sizes:[{size:'S'},{size:'M'},{size:'XXL'},],
+
+  },
+  {
+      title: "Chhath Theme Based Tshirt (Hindi) 3",
+      price: "400",
+      cost: "8000",
+      off: "520",
+      imageSrc: "products/t-shirt-festival.jpeg",
+      sizes:[{size:'S'},{size:'L'},{size:'XL'},],
+  },
+  {
+      title: "Chhath Theme Based Tshirt (Hindi) 20",
+      price: "400",
+      cost: "800",
+      off: "50",
+      imageSrc: "products/t-shirt-festival.jpeg",
+      sizes:[{size:'S'},{size:'M'},{size:'XXL'},],
+
+  },
+  {
+      title: "Chhath Theme Based Tshirt (Hindi) 30",
+      price: "400",
+      cost: "800",
+      off: "50",
+      imageSrc: "products/t-shirt-festival.jpeg",
+      sizes:[{size:'S'},{size:'L'},{size:'XL'},],
+  },
+]
+
+export default function Home(props:any) {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <Head>
-          <title>🖤 NextJS + TailwindCSS + Typescript + SASS + Dark Theme</title>
-        </Head>
-        <DarkToggle/>
-        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-          <h1 className="text-6xl font-bold">
-            Welcome to{' '}
-            <a className="text-blue-600" href="https://nextjs.org">
-              Next.js!
-            </a>
-          </h1>
-
-          <p className="mt-3 text-2xl">
-            Get started by editing{' '}
-            <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-              pages/index.js
-            </code>
-          </p>
-
-          <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-            <a
-              href="https://nextjs.org/docs"
-              className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-            >
-              <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-              <p className="mt-4 text-xl">
-                Find in-depth information about Next.js features and API.
-              </p>
-            </a>
-
-            <a
-              href="https://nextjs.org/learn"
-              className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-            >
-              <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-              <p className="mt-4 text-xl">
-                Learn about Next.js in an interactive course with quizzes!
-              </p>
-            </a>
-
-            <a
-              href="https://github.com/vercel/next.js/tree/master/examples"
-              className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-            >
-              <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-              <p className="mt-4 text-xl">
-                Discover and deploy boilerplate example Next.js projects.
-              </p>
-            </a>
-
-            <a
-              href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-            >
-              <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-              <p className="mt-4 text-xl">
-                Instantly deploy your Next.js site to a public URL with Vercel.
-              </p>
-            </a>
-          </div>
-        </main>
-
-        <footer className="flex items-center justify-center w-full h-24 border-t">
-          <a
-            className="flex items-center justify-center"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <div className="dark:bg-white px-2 py-1 ml-2 rounded">
-              <img src="/vercel.svg" alt="Vercel Logo" className="h-5" />
-            </div>
-          </a>
-        </footer>
+    <Layout>
+      <div className="grid grid-cols-12  gap-x-6 gap-y-8 p-2 lg:p-12">
+      {FeaturedProducts.map((item: any) => (
+        <>
+          <Card {...item}/>
+        </>
+      ))}
       </div>
+    </Layout>
     </>
   )
 }
